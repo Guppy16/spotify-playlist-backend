08faab5e52355720b0ad0c812237e5f255addfb2
Guppy16 <guptaalgebra@gmail.com>
2020-04-10 00:22:46 +0100

    Added DB functionality

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const bodyParser = require('body-parser');
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL + '?sslmode=require',
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
@@ -102,7 +102,8 @@ app.get('/db', async (req, res) => {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM test_table');
     const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
+    console.log(results);
+    // res.render('pages/db', results );
     client.release();
   } catch (err) {
     console.error(err);
