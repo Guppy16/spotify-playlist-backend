8504458afe434439077eeb8744ecc32f6e41a3cd
Guppy16 <guptaalgebra@gmail.com>
2020-04-10 04:09:50 +0100

    Testing db query

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ app.get('/callback', function(req, res) {
 
           // Search for spotifyID in table
           const result = await client.query(`SELECT COUNT(1) FROM users WHERE userspotifyid LIKE '${spotifyID}'`);
-          console.log(result);
+          console.log(result.rows.count);
           // Add person to table if necessary
-          if (!result){await client.query(`INSERT INTO users VALUES ('${spotifyID}', '${username}')`);}
+          if (!result.rows.count){await client.query(`INSERT INTO users VALUES ('${spotifyID}', '${username}')`);}
           client.release();
         } catch (err) {
           console.error(err);
